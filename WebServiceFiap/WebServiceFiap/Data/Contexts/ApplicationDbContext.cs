0e783de69c8255da736e3b46fce82204b21cce8b
Alberto Oliveira <alberto.bruno.s.oliveira.2004@gmail.com>
2026-06-12 21:21:21 -0300

    Sobe a primeira Migration

--- WebServiceFiap/WebServiceFiap/Data/Contexts/ApplicationDbContext.cs
+++ WebServiceFiap/WebServiceFiap/Data/Contexts/ApplicationDbContext.cs
@@ -7,6 +7,13 @@ namespace WebServiceFiap.Data.Contexts
     {
 
         public DbSet<UsuarioModel> Usuarios { get; set; }
+        public DbSet<CatadorItemModel> CatadoresItens { get; set; }
+        public DbSet<CatadorModel> Catadores { get; set; }
+        public DbSet<CentroColetaModel> CentrosColeta { get; set; }
+        public DbSet<ColetaItemModel> ColetasItens { get; set; }
+        public DbSet<ColetaModel> Coletas { get; set; }
+        public DbSet<DescartadorModel> Descartadores { get; set; }
+        public DbSet<ItemModel> Itens { get; set; }
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> opt) : base(opt)
         {
@@ -14,6 +21,16 @@ namespace WebServiceFiap.Data.Contexts
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            // Faz a conversão de BOOL para INT pois o Oracle Não suporta BOOL em Coluna
+            modelBuilder.Entity<ColetaModel>()
+                .Property(x => x.FoiFinalizada)
+                .HasConversion<int>();
+
+            modelBuilder.Entity<CatadorItemModel>()
+                .Property(x => x.FoiEntregue)
+                .HasConversion<int>();
+
+            // Cria chaves primárias compostas
             modelBuilder.Entity<ColetaItemModel>()
                 .HasKey(x => new
                 {
